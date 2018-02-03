518afa0f60536d8f36706bfbba211a223ac67429
abstructs <andrewclarke540@gmail.com>
2018-02-03 18:18:58 -0500

    query db works

--- fishii_api/app.py
+++ fishii_api/app.py
@@ -2,7 +2,12 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import g
+from flask import send_from_directory
+import sys
+import os
 import sqlite3 
+
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'public')
 
 DATABASE = 'fishbase.db'
 
@@ -20,7 +25,39 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.route("/")
-def hello():
+def index():
     db = get_db()
+    query_db("SELECT * FROM User")
     return jsonify(request.args)
+
+@app.route("/signup")
+def signup():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    return query_db("SELECT username FROM User WHERE username=?", username)
+
+    verify_user(username, password) 
+
+    if verify_user(username, password):
+        return "Success"
+    else:
+        return "failure"
+    db = get_db()
+    query_db("SELECT username FROM User")
+    return jsonify()
+
+def verify_user(username, password):
+    try:
+        if not(len(username) > 0 and len(username) < 16):
+            print("\n\n\n", file=sys.stderr)
+            
+    except TypeError:
+        return False
